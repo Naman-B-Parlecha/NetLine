ecfd64acac2385559db52ab50b16b3b40932883b
Vivek Agarwal <133840262+agarwalvivek29@users.noreply.github.com>
2024-09-04 12:22:08 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true, // Enable this if you're using the app directory with Next.js
+    },
+};
+
+export default nextConfig;
