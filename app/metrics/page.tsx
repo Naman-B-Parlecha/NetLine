8bc831ba85db574ab78ccf4a3dc2ba1c1aa2bee9
Naman-B-Parlecha <namanparlecha@gmail.com>
2025-01-13 15:23:42 +0530

    Grafana warning

--- app/metrics/page.tsx
+++ app/metrics/page.tsx
@@ -1,5 +1,24 @@
-export default function Grafana(){
-    return <div className="w-screen h-screen">
-                <iframe src="http://localhost:3000/d/fe6n6bcww7abka/lan-lords?orgId=1&from=now-6h&to=now&timezone=browser" className="h-screen w-full" ></iframe>
-            </div>
-}
+// export default function Grafana() {
+//   return (
+//     <div className="w-screen h-screen">
+//       <iframe
+//         src="http://localhost:3000/d/fe6n6bcww7abka/lan-lords?orgId=1&from=now-6h&to=now&timezone=browser"
+//         className="h-screen w-full"
+//       ></iframe>
+//     </div>
+//   );
+// }
+
+export default function Grafana() {
+    return (
+      <div className="w-screen h-screen bg-[#000022] text-white flex justify-center items-center">
+        {/* <iframe
+          src="http://localhost:3000/d/fe6n6bcww7abka/lan-lords?orgId=1&from=now-6h&to=now&timezone=browser"
+          className="h-screen w-full"
+        ></iframe> */}
+        <h1>GRAFANA DASHBOARD NOT IMPORTED</h1>
+        <p className="text-red-500">Please try again later...</p>
+      </div>
+    );
+  }
+  
